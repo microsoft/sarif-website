00c7cc3d1365affe9460575037892509cf090868
Larry Golding <larrygolding@comcast.net>
2018-08-25 14:42:58 -0700

    Introduce ValidationController Web API controller

---
+++ src/SarifWeb/Controllers/ValidationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SarifWeb.Controllers
+{
+    public class ValidationController : ApiController
+    {
+        // GET: api/Validation
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "SARIF Validation API is online" };
+        }
+
+        // POST: api/Validation
+        public void Post([FromBody]string value)
+        {
+        }
+    }
+}
