6eeeeb3ddf94471cfd107e3c717a77cb2c7df9ed
Larry Golding <larrygolding@comcast.net>
2018-08-30 14:16:43 -0700

    Refactor: extract HostingHelper.

--- src/SarifWeb/Controllers/ValidationController.cs
+++ src/SarifWeb/Controllers/ValidationController.cs
@@ -23,13 +23,13 @@ namespace SarifWeb.Controllers
 
         public ValidationController()
         {
-            string multitoolDirectory = HostingEnvironment.MapPath("~/bin/Sarif.Multitool");
-            string postedFilesDirectory = HostingEnvironment.MapPath("~/UploadedFiles");
+            string validationToolDirectory = HostingHelper.ValidationToolDirectory;
+            string postedFilesDirectory = HostingHelper.PostedFilesDirectory;
 
             IFileSystem fileSystem = new FileSystem();
             IProcessRunner processRunner = new ProcessRunner();
 
-            _validationService = new ValidationService(postedFilesDirectory, multitoolDirectory, fileSystem, processRunner);
+            _validationService = new ValidationService(postedFilesDirectory, validationToolDirectory, fileSystem, processRunner);
         }
 
         public async Task<ValidationResponse> Post([FromBody] ValidationRequest validationRequest)
