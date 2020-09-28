5ec0ebd45b34e28fe9f86042b3fcc3b9b7d756dd
Larry Golding <lgolding@microsoft.com>
2020-09-28 14:27:45 -0700

    Introduce a Selenium-based test of validation. (#121)
    We introduce a Selenium-based browser test that drives the validation experience end to end.
    
    Disclaimer: We do _not_ intend to introduce a large number of browser tests. They are part of a test strategy that will mostly consist of unit tests and functional tests. At the moment, this code base has precisely two unit tests. One appropriate time to use browser tests is precisely when a code base _lacks_ unit tests and functional tests. In that scenario, a small number of browser tests can at least give you some confidence that the site isn't completely broken.

---
+++ src/SarifWeb.UITests/PageTestBase.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SarifWeb.UITests
+{
+    public abstract class PageTestBase : IDisposable
+    {
+        private bool disposed;
+
+        protected PageTestBase()
+        {
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.LogPath = "chromedriver.log";
+            service.EnableVerboseLogging = true;
+
+            Driver = new ChromeDriver(service);
+        }
+
+        protected IWebDriver Driver { get; private set; }
+
+        #region IDispose
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    if (Driver != null)
+                    {
+                        Driver.Dispose();
+                        Driver = null;
+                    }
+                }
+
+                disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+    }
+}
