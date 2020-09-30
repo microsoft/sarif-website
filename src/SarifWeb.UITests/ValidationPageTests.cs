77fca2eca43feba090e001f3ec7fed1c9d5e2804
Larry Golding <lgolding@comcast.net>
2020-09-30 16:05:45 -0700

    Modify test file to produce SARIF2012; renders as "note".

--- src/SarifWeb.UITests/ValidationPageTests.cs
+++ src/SarifWeb.UITests/ValidationPageTests.cs
@@ -34,12 +34,12 @@ namespace SarifWeb.UITests
 
             page.ClickAdditionalSuggestions();
 
-            page.NumResults.Should().Be(5);
+            page.NumResults.Should().Be(6);
             page.CurrentResultIndex.Should().Be(1);
 
             page.ClickGitHubRules();
 
-            page.NumResults.Should().Be(7);
+            page.NumResults.Should().Be(8);
             page.CurrentResultIndex.Should().Be(1);
 
             page.ClickAdditionalSuggestions();
